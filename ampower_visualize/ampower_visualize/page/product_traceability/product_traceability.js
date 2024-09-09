37ffdba22d087117493cc3c7664d1b784ede6943
Sambhav Saxena <sambhav.saxena@ambibuzz.com>
2024-09-09 15:23:42 +0530

    enabled canvas events n selecting document and doctype
    Signed-off-by: Sambhav Saxena <sambhav.saxena@ambibuzz.com>

--- ampower_visualize/ampower_visualize/page/product_traceability/product_traceability.js
+++ ampower_visualize/ampower_visualize/page/product_traceability/product_traceability.js
@@ -7,7 +7,7 @@ frappe.pages['product_traceability'].on_page_load = function (wrapper) {
 	setup_fields(page, wrapper)
 };
 
-function setup_fields(page, wrapper) {
+const setup_fields = (page, wrapper) => {
 	let is_document_name_added = false;
 	let is_field_name_added = false;
 	let doctype_field = page.add_field({
@@ -37,23 +37,107 @@ function setup_fields(page, wrapper) {
 	});
 }
 
-function log_to_console() {
+const log_to_console = () => {
 	console.log("Log function called from appended html")
+}
+
+const makeDraggable = (element) => {
+	let isDragging = false;
+	let startX, startY;
+
+	element.addEventListener('mousedown', startDragging);
+	element.addEventListener('touchstart', startDragging);
+
+	document.addEventListener('mousemove', drag);
+	document.addEventListener('touchmove', drag);
+
+	document.addEventListener('mouseup', stopDragging);
+	document.addEventListener('touchend', stopDragging);
+
+	function startDragging(e) {
+		isDragging = true;
+		const rect = element.getBoundingClientRect();
+		startX = (e.clientX || e.touches[0].clientX) - rect.left;
+		startY = (e.clientY || e.touches[0].clientY) - rect.top;
+		e.preventDefault();
+	}
+
+	function drag(e) {
+		if (!isDragging) return;
+		const x = (e.clientX || e.touches[0].clientX) - canvasContainer.getBoundingClientRect().left;
+		const y = (e.clientY || e.touches[0].clientY) - canvasContainer.getBoundingClientRect().top;
+		element.style.left = ((x - startX) / scale) + 'px';
+		element.style.top = ((y - startY) / scale) + 'px';
+	}
+
+	function stopDragging() {
+		isDragging = false;
+	}
 }
 
 const append_base_html = (data, wrapper, document_name) => {
 	$(wrapper).find('.layout-main-section').append(`
-        <div class="row">
-            <div class="col-sm-9">
-                <div id="node_display" class="linked-list">The documents linked to ${document_name} are:</div>
-				<ul>
-				    ${data.map((doc) => `<li>${doc.linked_doctype} - ${doc.linked_parent}</li>`).join('')}
-				</ul>
-            </div>
-			<button onclick="log_to_console()">
-				LOG ON CONSOLE
-			</button>
-        </div>
+		<style>
+			#canvasContainer {
+				position: relative;
+				width: 100%;
+				height: 600px;
+				border: 1px solid #ccc;
+				overflow: hidden;
+			}
+
+			#contentWrapper {
+				position: absolute;
+				top: 0;
+				left: 0;
+				width: 100%;
+				height: 100%;
+				transform-origin: 0 0;
+			}
+
+			.embedded-div {
+				position: absolute;
+				padding: 10px;
+				background-color: #f0f0f0;
+				border: 1px solid #999;
+				cursor: move;
+				user-select: none;
+			}
+
+			#addButton {
+				margin-top: 10px;
+			}
+		</style>
+		<script>
+			let scale = 1;
+			const contentWrapper = document.getElementById('contentWrapper');
+			const canvasContainer = document.getElementById('canvasContainer');
+			const addButton = document.getElementById('addButton');
+			document.querySelectorAll('.embedded-div').forEach(makeDraggable);
+			canvasContainer.addEventListener('wheel', (e) => {
+				e.preventDefault();
+				const delta = e.deltaY > 0 ? 0.9 : 1.1;
+				scale *= delta;
+				contentWrapper.style.transform = \`scale(\${scale})\`;
+			});
+			addButton.addEventListener('click', () => {
+				const newDiv = document.createElement('div');
+				newDiv.className = 'embedded-div';
+				newDiv.style.top = '100px';
+				newDiv.style.left = '100px';
+				newDiv.textContent = 'New Div';
+				contentWrapper.appendChild(newDiv);
+				makeDraggable(newDiv);
+			});
+		</script>
+        <div id="canvasContainer">
+			<div id="contentWrapper">
+				<div class="embedded-div" style="top: 80px; left: 20px;">Embedded Div 1</div>
+				<div class="embedded-div" style="top: 100px; left: 150px;">Embedded Div 2</div>
+				<div class="embedded-div" style="top: 200px; left: 50px;">Embedded Div 3</div>
+			</div>
+		</div>
+		<button id="addButton">Add New Div</button>
     `);
 }
 
