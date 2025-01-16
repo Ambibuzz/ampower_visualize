891618c5c75e58f21fef76182881cc660377a25b
Sambhav Saxena <sambhav.saxena@ambibuzz.com>
2025-01-16 16:45:56 +0530

    fixed batch quantity field, added warehouse
    Signed-off-by: Sambhav Saxena <sambhav.saxena@ambibuzz.com>

--- ampower_visualize/ampower_visualize/page/batch_traceability/batch_traceability.py
+++ ampower_visualize/ampower_visualize/page/batch_traceability/batch_traceability.py
@@ -1,7 +1,7 @@
 import frappe
 
 @frappe.whitelist()
-def get_serial_and_batch_bundle_links(sabb_name):
+def get_serial_and_batch_bundle_links(sabb_name=""):
     try:
         serial_and_batch_bundle = frappe.get_doc("Serial and Batch Bundle", sabb_name)
     except frappe.DoesNotExistError:
@@ -18,9 +18,11 @@ def get_serial_and_batch_bundle_links(sabb_name):
     entries = serial_and_batch_bundle.get("entries")
     if isinstance(entries, list):
         for entry in entries:
+            batch_qty = frappe.get_value("Batch", entry.batch_no, "batch_qty")
             batch_links = {
                 "batch_no": entry.batch_no,
-                "batch_qty": entry.qty,
+                "batch_qty": batch_qty if batch_qty else 0,
+                "current_warehouse": entry.warehouse,
                 "serial_numbers": get_serial_numbers_for_batch(entry.batch_no),
             }
 
@@ -29,18 +31,16 @@ def get_serial_and_batch_bundle_links(sabb_name):
     return result
 
 def get_serial_numbers_for_batch(batch_no):
-    serial_numbers = []
-    sn_records = frappe.get_all(
+    serial_numbers = frappe.get_all(
         "Serial No",
         filters={"batch_no": batch_no},
         fields=["name", "item_code", "serial_no"]
     )
-
-    for sn in sn_records:
-        serial_numbers.append({
+    return [
+        {
             "serial_no": sn["serial_no"],
             "item_code": sn["item_code"],
             "unique_id": f"{sn['name']}-{sn['item_code']}-{sn['serial_no']}",
-        })
-
-    return serial_numbers
+        }
+        for sn in serial_numbers
+    ]
